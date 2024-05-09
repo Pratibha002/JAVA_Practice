491cf65854627f3e143cdcfa72a8754d413f5ab6
Pratibha <pratibhachouhan021@gmail.com>
2024-05-09 07:21:46 +0530

    find square of given no.

--- Que5.java
+++ Que5.java
@@ -1,14 +1,15 @@
 package JAVA_Practice;
 
 import java.util.Scanner;
+
 public class Que5 {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.print("ENTER THE NUMBER : "); 
-        int num = s.nextInt();
-        num = num*num;
-        System.out.println("THE SQUARE OF GIVEN NMBER IS = " +num);
-        s.close();    
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER : ");
+        int num = sc.nextInt();
+        num = num * num;
+        System.out.println("THE SQUARE OF GIVEN NMBER IS = " + num);
+        sc.close();
 
-    } 
+    }
 }
