d5f280f4ae4c6216f532a323fbfff59b8fb3cd04
Pratibha <pratibhachouhan021@gmail.com>
2024-05-09 16:46:28 +0530

    parameterized_constructor

---
+++ parameterized_constructor.java
@@ -0,0 +1,26 @@
+package JAVA;
+
+class A {
+    int x;
+    double y;
+    String z;
+
+    A(int a, double d, String s) {
+        x = a;
+        y = d;
+        z = s;
+
+    }
+
+    void show() {
+        System.out.println(x + " " + y + " " + z);
+    }
+}
+
+public class parameterized_constructor {
+    public static void main(String[] args) {
+        A ref = new A(10, 10.90, "Hello");
+        ref.show();
+
+    }
+}
