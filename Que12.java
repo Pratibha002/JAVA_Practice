f4c6cb9f9d37bbc0509e2cdef4b1147005f4dd32
Pratibha <pratibhachouhan021@gmail.com>
2024-05-09 07:43:14 +0530

    Counting numbers of a's coming in string

--- Que12.java
+++ Que12.java
@@ -2,18 +2,18 @@ package JAVA_Practice;
 
 public class Que12 {
     public static void main(String[] args) {
-        String s ="IndiaIsaGreatCountry";
-        
-        int count=0;
-        for(int i=0; i<s.length(); i++){
-           
-            if(s.charAt(i)=='a'){
-               
-               count++;
-               
+        String S = "IndiaIsaGreatCountry";
+
+        int count = 0;
+        for (int i = 0; i < S.length(); i++) {
+
+            if (S.charAt(i) == 'a') {
+
+                count++;
+
             }
-             
+
         }
-        System.out.println("total number of a = " +count);
+        System.out.println("total number of a = " + count);
     }
 }
