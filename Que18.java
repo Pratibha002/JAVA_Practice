dbc91c85821ba5f032185dec7f50ee15bbc1e01b
Pratibha <pratibhachouhan021@gmail.com>
2024-05-09 07:58:00 +0530

    reverse of a number

--- Que18.java
+++ Que18.java
@@ -2,7 +2,7 @@ package JAVA_Practice;
 
 public class Que18 {
     public static void main(String[] args) {
-        for(int i=10; i>=0; i--){
+        for(int i=10; i>=0; i--){ //reverse of a number
             System.out.println(i);
         }
     }
