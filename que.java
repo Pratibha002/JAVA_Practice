f535f6b125edd868f0dc87dafd68ff6ae1445599
Pratibha <pratibhachouhan021@gmail.com>
2024-05-09 07:12:34 +0530

    odd numbers

--- que.java
+++ que.java
@@ -1,18 +1,17 @@
 package JAVA_Practice;
 
 import java.util.Scanner;
+
 public class que {
     public static void main(String[] args) {
-        Scanner s =new Scanner(System.in);
-        System.out.print("Enter the number = " );
-        int num =s.nextInt();
-        for(int i = 1; i <=num; i++){
-            if(i % 2 != 0){
-                System.out.println("odd number = " +i);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number = ");
+        int num = sc.nextInt();
+        for (int i = 1; i <= num; i++) {
+            if (i % 2 != 0) {
+                System.out.println("odd number = " + i);
             }
         }
-        s.close();
+        sc.close();
     }
 }
-
-
