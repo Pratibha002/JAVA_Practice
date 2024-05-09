4d50eadce5f6d59d6b4ae850c7c2e479890096e6
Pratibha <pratibhachouhan021@gmail.com>
2024-05-09 07:34:36 +0530

    Pattern (print 1 to 10 numbers in tree pattern)

--- Que8.java
+++ Que8.java
@@ -3,8 +3,8 @@ package JAVA_Practice;
 public class Que8 {
     public static void main(String[] args) {
         int sum = 0;
-        for (int row = 1; row <= 4; row++) {
-            for (int column = 1; column <= row; column++) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
                 sum = sum + 1;
                 System.out.print(sum + " ");
 
