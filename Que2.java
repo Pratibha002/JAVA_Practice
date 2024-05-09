309cf109ac1da9607cdbfd0e88c66a91e3a77ba8
Pratibha <pratibhachouhan021@gmail.com>
2024-05-09 07:17:28 +0530

    sum of n natural numbers

--- Que2.java
+++ Que2.java
@@ -1,17 +1,20 @@
 //SUM of n numbers!!
 
 package JAVA_Practice;
+
 import java.util.Scanner;
+
 public class Que2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int sum =0;
+        Scanner s = new Scanner(System.in);
+        int sum = 0;
         System.out.println("Enter the natural number : ");
-        int num = sc.nextInt();
-        for(int i = 0; i<=num; i++){
-            sum=sum+i;
+        int num = s.nextInt();
+        for (int i = 0; i <= num; i++) {
+            sum = sum + i;
 
-        }System.out.println("THE SUM OF NATURAL NUMBER TILL "+num+" is = " +sum);
-      sc.close();
+        }
+        System.out.println("THE SUM OF NATURAL NUMBER TILL " + num + " is = " + sum);
+        s.close();
     }
 }
