08471667408546e1fcaecf6e15617093967005b2
Pratibha <pratibhachouhan021@gmail.com>
2024-05-09 07:36:44 +0530

    Pattern (reverse)

--- Que9.java
+++ Que9.java
@@ -2,17 +2,16 @@ package JAVA_Practice;
 
 public class Que9 {
     public static void main(String[] args) {
-        int sum =11;
-        for(int i =4 ; i>=1; i--){
-            for(int j=1; j<=i; j++){
-                sum=sum-1;
-                System.out.print(sum+" ");
-             
-                
+        int sum = 11;
+        for (int row = 4; row >= 1; row--) {
+            for (int column = 1; column <= row; column++) {
+                sum = sum - 1;
+                System.out.print(sum + " ");
+
             }
             System.out.println("  ");
-            
+
         }
-        
+
     }
 }
