0127f4dc106f194c2121c9fe59bb1c0012369ce1
Pratibha <pratibhachouhan021@gmail.com>
2024-05-09 07:25:21 +0530

    Pattern '1'

--- Que7.java
+++ Que7.java
@@ -2,11 +2,12 @@ package JAVA_Practice;
 
 public class Que7 {
     public static void main(String[] args) {
-        for(int i =1 ; i<=4; i++){
-            for(int j=1; j<=i; j++){
-                System.out.print(1); 
-                 
-            }System.out.println("  ");
+        for (int row = 1; row <= 4; row++) {
+            for (int column = 1; column <= row; column++) {
+                System.out.print(1);
+
+            }
+            System.out.println("  ");
         }
     }
 }
