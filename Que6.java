edb7356b2c294e7cc38e283b9a0b9e97ff136b0b
Pratibha <pratibhachouhan021@gmail.com>
2024-05-09 07:23:07 +0530

    find cube of given no.

--- Que6.java
+++ Que6.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Que6 {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.print("ENTER THE NUMBER : "); 
-        int num = s.nextInt();
-        num = num*num*num;
-        System.out.println("THE CUBE OF GIVEN NMBER IS = " +num);
-        s.close();    
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER : ");
+        int num = sc.nextInt();
+        num = num * num * num;
+        System.out.println("THE CUBE OF GIVEN NMBER IS = " + num);
+        sc.close();
 
-    } 
+    }
 }
