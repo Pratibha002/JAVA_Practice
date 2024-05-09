e139a874d8e4e4aa6ca06e5fb550ed6878b26532
Pratibha <pratibhachouhan021@gmail.com>
2024-05-09 07:46:01 +0530

    sorting three numbers in ascending order

--- Que13_1.java
+++ Que13_1.java
@@ -4,51 +4,52 @@ import java.util.Scanner;
 
 public class Que13_1 {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.print("Enter a number1 = ");
-        int num1 = sc.nextInt();
+        int num1 = s.nextInt();
         System.out.print("Enter a number2 = ");
-        int num2 =  sc.nextInt();
+        int num2 = s.nextInt();
         System.out.print("Enter a number3 = ");
-        int num3 =  sc.nextInt();
+        int num3 = s.nextInt();
 
-        if((num1>num2)&&(num1>num3)){
-         if(num2>num3){
-            System.out.println("largest number = " +num1);
-            System.out.println("middle number = " +num2);
-            System.out.println("smallest number = " +num3);}
-            if(num2<num3){
-                System.out.println("largest number = " +num1);
-                System.out.println("smallest number = " +num3);
-                System.out.println("middle number = " +num2);
-                  
+        if ((num1 > num2) && (num1 > num3)) {
+            if (num2 > num3) {
+                System.out.println("largest number = " + num1);
+                System.out.println("middle number = " + num2);
+                System.out.println("smallest number = " + num3);
             }
-         
+            if (num2 < num3) {
+                System.out.println("largest number = " + num1);
+                System.out.println("smallest number = " + num3);
+                System.out.println("middle number = " + num2);
+
+            }
+
+        } else if ((num2 > num3) && (num2 > num1)) {
+            if (num3 > num1) {
+                System.out.println("largest number = " + num2);
+                System.out.println(" middle number = " + num3);
+                System.out.println("smallest number = " + num1);
+            }
+            if (num3 < num1) {
+                System.out.println("largest number = " + num2);
+                System.out.println(" middle number = " + num1);
+                System.out.println("smallest number = " + num3);
+            }
+
+        } else if ((num3 > num1) && (num3 > num2)) {
+            if (num1 > num2) {
+                System.out.println("largest number = " + num3);
+                System.out.println(" middle number = " + num1);
+                System.out.println("smallest number = " + num2);
+            }
+            if (num1 < num2) {
+                System.out.println("largest number = " + num3);
+                System.out.println(" middle number = " + num2);
+                System.out.println("smallest number = " + num1);
+            }
+
         }
-        else if((num2>num3)&&(num2>num1)){
-          if(num3>num1){
-            System.out.println("largest number = " +num2);
-            System.out.println(" middle number = " +num3);
-            System.out.println("smallest number = " +num1);}
-            if(num3<num1){
-                System.out.println("largest number = " +num2);
-                System.out.println(" middle number = " +num1);
-                System.out.println("smallest number = " +num3);
-            }
-          
-        }
-        else if((num3>num1)&&(num3>num2)){
-         if(num1>num2){
-            System.out.println("largest number = " +num3);
-            System.out.println(" middle number = " +num1);
-            System.out.println("smallest number = " +num2);}
-            if(num1<num2){
-                System.out.println("largest number = " +num3);
-                System.out.println(" middle number = " +num2);
-                System.out.println("smallest number = " +num1);
-            }
-         
-        } 
-        sc.close();
-}
+        s.close();
+    }
 }
