976770e73d95a8bf60556317ed69a3e79f632a10
Pratibha <pratibhachouhan021@gmail.com>
2024-05-09 07:53:36 +0530

    even and odd

--- Que15.java
+++ Que15.java
@@ -2,14 +2,13 @@ package JAVA_Practice;
 
 public class Que15 {
     public static void main(String[] args) {
-        for(int i =0; i <= 15; i++){
-           if(i%2==0){
-            System.out.println(+i+" IS EVEN NUMBER!!");
-           }
-           else{
-            System.out.println(+i+" IS ODD NUMBER!!");
-           }
+        for (int i = 0; i <= 15; i++) {
+            if (i % 2 == 0) { // check even and odd
+                System.out.println(+i + " IS EVEN NUMBER!!");
+            } else {
+                System.out.println(+i + " IS ODD NUMBER!!");
+            }
         }
     }
-    
+
 }
