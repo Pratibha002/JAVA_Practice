82db556fc1c3be007987d12b01a1fb3f33fea147
Pratibha <pratibhachouhan021@gmail.com>
2024-05-09 07:57:01 +0530

    checking multiples of 3 and 5

--- Que16.java
+++ Que16.java
@@ -1,17 +1,17 @@
 package JAVA_Practice;
 
 public class Que16 {
-   public static void main(String[] args) {
-    for(int i = 1 ; i<=100 ; i++){
-        if(i%3==0){
-            System.out.println(i+" = FIZZ");
-        }
-        if((i%3==0)&&(i%5==0)){
-            System.out.println(i+" = FizzBuzz");
-        }
-        if(i%5==0){
-            System.out.println(i+" = Buzz");
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) { // divisible by 3
+                System.out.println(i + " = FIZZ");
+            }
+            if ((i % 3 == 0) && (i % 5 == 0)) { // divisible by both 3 and 5
+                System.out.println(i + " = FizzBuzz");
+            }
+            if (i % 5 == 0) {// divisible by 5
+                System.out.println(i + " = Buzz");
+            }
         }
     }
-   } 
 }
