41ef0de5a661c81c423d8c0726e7269afb81143f
Pratibha <pratibhachouhan021@gmail.com>
2024-05-13 09:49:37 +0530

    Implemented parameterized methods for addition and subtraction with user input

--- Prameterized_method.java
+++ Prameterized_method.java
@@ -1,13 +1,16 @@
 package JAVA_Practice;
 
 import java.util.Scanner;
+
 public class Prameterized_method {
-    public int add(int a , int b){
-        return a+b;
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int sub(int a , int b){
-        return a-b;
+
+    public int sub(int a, int b) {
+        return a - b;
     }
+
     public static void main(String[] args) {
         Prameterized_method p = new Prameterized_method();
         try (Scanner s = new Scanner(System.in)) {
@@ -16,14 +19,13 @@ public class Prameterized_method {
             System.out.println("ENTER THE VALUE OF NUM2: ");
             int num2 = s.nextInt();
 
-            int r1=p.add(num1, num2);
-            int r2=p.sub(num1, num2);
+            int r = p.add(num1, num2);
+            int r1 = p.sub(num1, num2);
 
-            System.out.println("NUM1 + NUM2 =  " +r1);
-            System.out.println("NUM1 - NUM2 = "  +r2);
+            System.out.println("NUM1 + NUM2 =  " + r);
+            System.out.println("NUM1 - NUM2 = " + r1);
         }
 
-
     }
-    
+
 }
