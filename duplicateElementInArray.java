9e3b11f1e5df5e3b5d155925969e91f184f5b8df
Pratibha <pratibhachouhan021@gmail.com>
2024-05-15 15:19:29 +0530

    print unique elements from a given array

---
+++ duplicateElementInArray.java
@@ -0,0 +1,31 @@
+package JAVA_Practice;
+
+public class duplicateElementInArray {
+    public static void main(String[] args) {
+        int a[] ={2,3,4,4,35,67,65,1,1,9,3};
+        for (int i=0; i<a.length;i++){
+           // System.out.print("GIVEN ARRAY "+a[i] +"  ");
+             boolean unique =true;
+            
+            for(int j =0; j<i; j++){
+                if(a[i]==a[j]){
+                    unique =false;
+                   // break;
+                }   
+            }
+            if(unique){
+           
+                System.out.println("UNIQUE NUMBER!!" +a[i]);
+            }
+        }  
+        }        
+    }
+             
+            
+           
+        
+   
+
+    
+    
+
