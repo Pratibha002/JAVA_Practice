8112a7a74235aadbbfe77e0762577260f212d882
Pratibha <pc634805@gmail.com>
2024-05-19 23:38:09 +0530

    Find Maximum element in array

--- ArrayMaxElement.java
+++ ArrayMaxElement.java
@@ -3,7 +3,7 @@ package JAVA_Practice;
 public class ArrayMaxElement {
     public static void main(String[] args) {
         int arr[]={3,5,4,1,9};
-        int arr2[]={22,14,8,17,35,3};
+        int arr_2[]={22,14,8,17,35,3};
         System.out.print("GIVEN ARRAY :  "); 
         int maxElement = arr[0];
         
@@ -18,11 +18,11 @@ public class ArrayMaxElement {
 
 
          System.out.print("GIVEN ARRAY :  "); 
-         for(int i = 0; i<arr2.length; i++){
-            System.out.print(arr2[i] +" ");
+         for(int i = 0; i<arr_2.length; i++){
+            System.out.print(arr_2[i] +" ");
             
-            if(maxElement<arr2[i]){
-                maxElement=arr2[i];
+            if(maxElement<arr_2[i]){
+                maxElement=arr_2[i];
             }
          } System.out.println();
          System.out.print("MAXIMUM ELEMENT :  " +maxElement);
