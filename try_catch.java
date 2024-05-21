3c6528e9f28a42dde0a2ee476df8886e6d13ae75
Pratibha <pc634805@gmail.com>
2024-05-21 22:29:37 +0530

    try, catch and finally

--- try_catch.java
+++ try_catch.java
@@ -6,17 +6,17 @@ public class try_catch {
             System.out.println("try 1");
             System.out.println("try 2");
             char c = 0;
-            int a=4/c;
+            int a=4/c;                                //risky code
             System.out.println("try 3");
             System.out.println("try 4");
 
         }catch(Exception exception){
-            System.out.println("catch 1" );
+            System.out.println("catch 1" );            //handling code
             System.out.println("catch 2");
 
             
 
-        }finally{
+        }finally{ //this block must be execute/run
             System.out.println("final 1");
             System.out.println("final 1");
         }
