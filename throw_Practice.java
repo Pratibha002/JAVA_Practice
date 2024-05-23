f0daf3e4a24497deb76d17850a3087c3a54586cb
Pratibha <pc634805@gmail.com>
2024-05-23 15:33:31 +0530

    throw keyword

---
+++ throw_Practice.java
@@ -0,0 +1,42 @@
+package JAVA_Practice;
+
+public class throw_Practice {
+    public static void main(String[] args) {
+        m1();
+        System.out.println("----------------------------------------------------");
+        m2();
+    }
+    public static void m1(){
+        try{
+        System.out.println("Statement 1");
+        int a=0;
+        int b=2/a;
+        System.out.println("Statement 2");
+        }catch(Exception e){
+            System.out.println("Statement 3");
+        }
+        finally{
+            System.out.println("Statement 4");
+        }
+        System.out.println("Statement 5");
+    }
+    public static void m2(){
+        try{
+        System.out.println("Statement 1");
+        int a=0;
+        int b=2/a;
+        System.out.println("Statement 2");
+        }catch(Exception e){
+            throw e;
+        }
+        finally{
+            System.out.println("Statement 4");
+        }
+        System.out.println("Statement 5");
+        }
+            
+    
+    
+}
+
+
