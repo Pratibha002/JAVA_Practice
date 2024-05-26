e4754f4c13fc872b7a3e718008e90399d4b9b470
Pratibha <pc634805@gmail.com>
2024-05-26 23:48:41 +0530

    AccessModifier-> calling methods from another class &  file within same package

---
+++ AccessModifier2.java
@@ -0,0 +1,12 @@
+package JAVA_Practice;
+
+
+public class AccessModifier2 extends Parent3{
+     public static void main(String[] args) {
+        AccessModifier2 a = new AccessModifier2();
+        a.m3();
+        
+    }
+
+    
+}
