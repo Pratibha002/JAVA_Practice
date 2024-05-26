170c24e297e117e0af8bfc9762553b2e92e65e65
Pratibha <pc634805@gmail.com>
2024-05-27 00:26:37 +0530

    StaticVariable

--- StaticVariable.java
+++ StaticVariable.java
@@ -2,6 +2,7 @@ package JAVA_Practice;
 
 public class StaticVariable {
     public static String name;
+   // it's not mendatory to intialise the static variable,because they are automatically initialized to their default values.
 
     public int m1(){
       // int a;
