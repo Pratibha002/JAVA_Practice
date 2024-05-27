fb30b0de23cbb3b4e2758c45135412d07b701ddb
Pratibha <pc634805@gmail.com>
2024-05-27 23:32:03 +0530

    Student

---
+++ Student4.java
@@ -0,0 +1,9 @@
+package JAVA_Practice;
+
+public class Student4 extends Person {
+    @Override
+  public void display() throws Exception 
+  {
+      System.out.println(" I am a Student ");
+  }
+}
