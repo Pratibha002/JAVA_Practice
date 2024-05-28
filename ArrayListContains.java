72bb478c7fb159036ba1756a7e3d96aeef543f1f
Pratibha <pc634805@gmail.com>
2024-05-28 23:00:01 +0530

    ArrayListContains

--- ArrayListContains.java
+++ ArrayListContains.java
@@ -1,2 +1,18 @@
-package JAVA_Practice;public class ArrayListContains {
-}
+package JAVA_Practice;
+import java.util.ArrayList;
+public class ArrayListContains {
+    public static void main(String[] args) {
+            ArrayList<String> student = new ArrayList<String>();
+            student.add("Akansha");
+            student.add("Madhuri");
+            student.add("Bhanu");
+            student.add("Ruchi");
+            student.add("Rahul");
+            student.add("Ayushi");
+            student.add("Pratibha");
+            System.out.println(student.contains("Aman Bhaiya"));
+            System.out.println(student.contains("Akansha"));
+
+        }
+    }
+
