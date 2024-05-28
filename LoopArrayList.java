73047a869fe7510c20b8ceb0329e2d5b8cefe4bf
Pratibha <pc634805@gmail.com>
2024-05-28 23:01:57 +0530

    LoopArrayList

--- LoopArrayList.java
+++ LoopArrayList.java
@@ -1,2 +1,17 @@
-package JAVA_Practice;public class LoopArrayList {
-}
+package JAVA_Practice;
+import java.util.ArrayList;
+public class LoopArrayList {
+        public static void main(String[] args) {
+            ArrayList<String> fruits= new ArrayList<String>();
+            fruits.add("Apple");
+            fruits.add("Avocado");
+            fruits.add("Banana");
+            fruits.add("Kiwi");
+            fruits.add("Mango");
+            for(int i=0; i<fruits.size(); i++){
+                System.out.println(fruits.get(i));
+            }
+
+        }
+    }
+
