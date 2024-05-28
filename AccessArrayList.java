9863b20501ab3e97785b78ae1a80d376ea17723c
Pratibha <pc634805@gmail.com>
2024-05-28 22:58:58 +0530

    AccessArrayList

--- AccessArrayList.java
+++ AccessArrayList.java
@@ -1,2 +1,18 @@
-package JAVA_Practice;public class AccessArrayList {
-}
+package JAVA_Practice;
+import java.util.ArrayList;
+public class AccessArrayList {
+        public static void main(String[] args) {
+            ArrayList <String> s =new ArrayList<String>();
+            s.add("Aman bhaiya");
+            s.add("Anurag bhaiya");
+            s.add("Bhanu");
+            s.add("Ruchi");
+            s.add("Pratibha");
+            System.out.println(s.get(0));//by it we can access the value of any particular index
+            System.out.println(s);
+
+
+
+        }
+    }
+
