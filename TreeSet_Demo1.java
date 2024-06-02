f480e9e784526b657e9076e3932b8566a0031010
Pratibha <pc634805@gmail.com>
2024-06-02 23:30:09 +0530

    TreeSet

--- TreeSet_Demo1.java
+++ TreeSet_Demo1.java
@@ -1,2 +1,20 @@
-package JAVA_Practice;public class TreeSet_Demo1 {
+package JAVA_Practice;
+
+import java.util.TreeSet;
+
+public class TreeSet_Demo1 {
+    public static void main (String[]args){
+        TreeSet<StringBuffer> s =new TreeSet<StringBuffer>();
+        s.add(new StringBuffer("A"));
+        s.add(new StringBuffer("B"));
+        s.add(new StringBuffer("Z"));
+        s.add(new StringBuffer("L"));
+        System.out.println(s);
+        System.out.println("A".compareTo("Z"));//-ve
+        System.out.println("Z".compareTo("B"));//+ve
+        System.out.println("A".compareTo("A"));//0
+        System.out.println("A".compareTo("null"));//NullPointerException
+
+
+    }
 }
