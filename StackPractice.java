db0111b1912f062904211a0f4fa2baee2d8d41eb
Pratibha <pc634805@gmail.com>
2024-06-02 23:28:52 +0530

    StackPractice

--- StackPractice.java
+++ StackPractice.java
@@ -1,2 +1,18 @@
-package JAVA_Practice;public class StackPractice {
+package JAVA_Practice;
+
+import java.util.*;
+
+public class StackPractice {
+    public static void main(String [] args ){
+        Stack <String> s = new Stack<String>();
+        s.push("A");
+        s.push("B");
+        s.push("C");
+        System.out.println(s);
+        System.out.println(s.search("A"));
+        System.out.println(s.search("Z"));
+        System.out.println(s.empty());
+
+
+    }
 }
