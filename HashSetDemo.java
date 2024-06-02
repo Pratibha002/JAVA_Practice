411b98f57ef404db5908aa1a7252865d78bfdfb0
Pratibha <pc634805@gmail.com>
2024-06-02 23:25:53 +0530

    HashSet

--- HashSetDemo.java
+++ HashSetDemo.java
@@ -1,2 +1,20 @@
-package JAVA_Practice;public class HashSetDemo {
+package JAVA_Practice;
+
+import java.util.*;
+
+public class HashSetDemo {
+    public static void main (String[] args){
+        HashSet<Object> h =new HashSet<Object>();
+        h.add("A");
+        h.add("B");
+        h.add("C");
+        h.add("D");
+        h.add(null);
+        h.add(10);
+        System.out.println(h);//insertion order is not preserved.
+        System.out.println(h.add("D"));//duplicates are not allowed. If we are trying to insert duplicates, we
+        //won't get any compile time or runtime errors. add() method simply return false
+        System.out.println(h);
+
+    }
 }
