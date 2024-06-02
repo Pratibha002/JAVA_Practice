84d7454f27587aaadd2fb47dabe842f735963cf0
Pratibha <pc634805@gmail.com>
2024-06-02 23:23:53 +0530

    Iterator

--- Cursor_Iterator.java
+++ Cursor_Iterator.java
@@ -1,2 +1,22 @@
-package JAVA_Practice;public class Cursor_Iterator {
+package JAVA_Practice;
+import java.util.*;
+public class Cursor_Iterator {
+
+    public static void main(String [] args) {
+        ArrayList<Integer> l = new ArrayList<Integer>();
+        for (int i = 0; i <= 10; i++) {
+            l.add(i);
+        }
+        System.out.println(l);
+        Iterator<Integer> itr = l.iterator();//it is used with all collections
+        while (itr.hasNext()) {
+            Integer I = (Integer) itr.next();//here we perform typeCasting because e.nextElement return type is object
+            if ((I % 2) == 0) {
+                System.out.println(I);
+            }else
+                itr.remove();
+        }
+        System.out.println(l);
+    }
+
 }
