d2885726d60e3f83cd138b5885f863391254c3fe
Pratibha <pc634805@gmail.com>
2024-06-06 23:08:20 +0530

    Java Program to Check if a Given Integer is Odd or Even

--- Basic_Programming/CheckEvenOdd.java
+++ Basic_Programming/CheckEvenOdd.java
@@ -1,2 +1,16 @@
-package JAVA_Practice.Basic_Programming;public class CheckEvenOdd {
+//Java Program to Check if a Given Integer is Odd or Even
+package JAVA_Practice.Basic_Programming;
+import java.util.Scanner;
+public class CheckEvenOdd {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("ENTER THE NUMBER : ");
+        int num =sc.nextInt();
+        if(num%2==0){
+            System.out.println("Given number "+num+" is even number ");
+        }else{
+            System.out.println("Given number "+num+" is odd number ");
+        }
+
+    }
 }
