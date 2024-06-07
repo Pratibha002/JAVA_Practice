84d204ffb2f20c996beb27675ff8dd3d5497f956
Pratibha <pc634805@gmail.com>
2024-06-07 23:23:43 +0530

    check given number is prime number or not.

--- Basic_Programming/PrimeNumber2.java
+++ Basic_Programming/PrimeNumber2.java
@@ -1,2 +1,30 @@
-package JAVA_Practice.Basic_Programming;public class PrimeNumber2 {
+//check given number is prime number or not.
+package JAVA_Practice.Basic_Programming;
+
+import java.util.Scanner;
+
+public class PrimeNumber2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        if (num <= 1) {
+            System.out.println("Given number " + num + " is not a Prime number.");
+        } else {
+            boolean isPrime = true;
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println("Given number " + num + " is a Prime number.");
+            } else {
+                System.out.println("Given number " + num + " is not a Prime number.");
+            }
+
+
+        }
+    }
 }
