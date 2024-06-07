02755979e727c85120678ab89df1cf54d8d9cf88
Pratibha <pc634805@gmail.com>
2024-06-07 23:24:39 +0530

    Java Program to Find if a Given Year is a Leap Year

--- Basic_Programming/LeapYear.java
+++ Basic_Programming/LeapYear.java
@@ -1,2 +1,15 @@
-package JAVA_Practice.Basic_Programming;public class LeapYear {
+//Java Program to Find if a Given Year is a Leap Year
+package JAVA_Practice.Basic_Programming;
+import java.util.Scanner;
+public class LeapYear {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num = sc.nextInt();
+        if(num%4==0){
+            System.out.println("Given Year " +num+ " is a Leap Year");
+        }else{
+            System.out.println("Given Year " +num+ " is Not a Leap Year");
+        }
+    }
 }
