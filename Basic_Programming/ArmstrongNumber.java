ce560b176231eda4bf4a14a039e2edcc2aee586c
Pratibha <pc634805@gmail.com>
2024-06-08 23:45:33 +0530

    Java Program to Check Armstrong Number

--- Basic_Programming/ArmstrongNumber.java
+++ Basic_Programming/ArmstrongNumber.java
@@ -1,2 +1,24 @@
-package JAVA_Practice.Basic_Programming;public class ArmstrongNumber {
+//Java Program to Check Armstrong Number
+package JAVA_Practice.Basic_Programming;
+import java.util.Scanner;
+public class ArmstrongNumber {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the initial number a :");
+        int a=sc.nextInt();
+        System.out.println("Enter the number b :");
+        int b=sc.nextInt();
+
+        for(int i=a; i<=b; i++){
+            int num=i;
+            int sum=0;
+            while (num>0){
+                int remainder=num%10;
+                sum=sum+(remainder*remainder*remainder);
+                num=num/10;
+            }if (sum==i){
+                System.out.println(sum);
+            }
+        }
+    }
 }
