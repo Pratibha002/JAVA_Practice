f39a340f74e3c5d2eb708337563a72dbaf88e64e
Pratibha <pc634805@gmail.com>
2024-06-09 22:04:49 +0530

    FibonacciSeriesSum

--- Basic_Programming/FibonacciSeriesSum.java
+++ Basic_Programming/FibonacciSeriesSum.java
@@ -1,2 +1,23 @@
-package JAVA_Practice.Basic_Programming;public class FibonacciSeriesSum {
+package JAVA_Practice.Basic_Programming;
+
+import java.util.Scanner;
+
+public class FibonacciSeriesSum {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num = sc.nextInt();
+        int a=0;
+        int b=1;
+        int nextTerm=0;
+        System.out.print("Fibonacci Series  : ");
+        for(int i=0; i<=num; i++){
+            System.out.print(a+" ");
+             nextTerm=a+b;
+             a=b;
+             b=nextTerm;
+
+        }System.out.println(" ");
+        System.out.println("Sum of fibonacci series = "+nextTerm);
+    }
 }
