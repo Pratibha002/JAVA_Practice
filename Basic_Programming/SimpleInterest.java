89a39333a7a152ee9cb2bd36d6352e60050cd8b2
Pratibha <pc634805@gmail.com>
2024-06-10 23:33:37 +0530

    Java Program to Calculate Simple Interest

--- Basic_Programming/SimpleInterest.java
+++ Basic_Programming/SimpleInterest.java
@@ -1,2 +1,19 @@
-package JAVA_Practice.Basic_Programming;public class SimpleInterest {
+//Java Program to Calculate Simple Interest
+package JAVA_Practice.Basic_Programming;
+
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER THE Principal : ");
+        double P = sc.nextDouble();
+        System.out.println("ENTER THE Rate : ");
+        double R = sc.nextDouble();
+        System.out.println("ENTER THE Time : ");
+        double T = sc.nextDouble();
+        double  SI=(P*R*T)/100;
+        System.out.println("SIMPLE INTEREST = "+SI);
+
+    }
 }
