4bc89812d3a5d1a4dd3e91bcb971ce635e3ce17f
Pratibha <pc634805@gmail.com>
2024-06-10 23:37:21 +0530

    PyramidNumberPattern

--- Pattern/PyramidNumberPattern.java
+++ Pattern/PyramidNumberPattern.java
@@ -1,2 +1,18 @@
-package JAVA_Practice.Pattern;public class PyramidNumberPattern {
+//     1
+//    2  2
+//   3  3  3
+//  4  4  4  4
+// 5  5  5  5  5
+package JAVA_Practice.Pattern;
+
+public class PyramidNumberPattern {
+    public static void main(String[] args){
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=5-i; j++){
+                System.out.print(" ");
+            }for(int j=1; j<=i; j++){
+                System.out.print(" "+ i+" ");
+            }System.out.println();
+        }
+    }
 }
