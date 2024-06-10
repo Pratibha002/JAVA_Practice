6234697050cde819d0ce348ebace3290c9077583
Pratibha <pc634805@gmail.com>
2024-06-10 23:32:28 +0530

    Perimeter Of Rectangle

--- Basic_Programming/PerimeterOfRectangle.java
+++ Basic_Programming/PerimeterOfRectangle.java
@@ -1,2 +1,15 @@
-package JAVA_Practice.Basic_Programming;public class PerimeterOfRectangle {
+package JAVA_Practice.Basic_Programming;
+
+import java.util.Scanner;
+
+public class PerimeterOfRectangle {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER THE LENGTH : ");
+        double L = sc.nextDouble();
+        System.out.println("ENTER THE BREADTH : ");
+        double B = sc.nextDouble();
+        double perimeter=2*(L+B);
+        System.out.println("Perimeter of a Rectangle = "+perimeter);
+    }
 }
