1547f316c62d4f43b41596de534152ed97cfa670
Pratibha <pc634805@gmail.com>
2024-06-11 23:32:10 +0530

    Pattern/ReversePyramidStarPattern.java

--- Pattern/ReversePyramidStarPattern.java
+++ Pattern/ReversePyramidStarPattern.java
@@ -1,2 +1,14 @@
-package JAVA_Practice.Pattern;public class ReversePyramidStarPattern {
+package JAVA_Practice.Pattern;
+
+public class ReversePyramidStarPattern {
+    public static void main(String[] args){
+        for(int i=7; i>=1; i--){
+            for(int j=7; j>i; j--){
+                System.out.print("  ");
+            }for(int j=1; j<=i; j++){
+                System.out.print(" *  ");
+            }
+            System.out.println();
+        }
+    }
 }
