6b6a2926e1cc6e9f7354f95cd5f0f7137ebe66bd
Pratibha <pc634805@gmail.com>
2024-06-11 23:34:05 +0530

    PyramidNumberPattern

--- Pattern/PyramidNumberPattern.java
+++ Pattern/PyramidNumberPattern.java
@@ -8,7 +8,7 @@ package JAVA_Practice.Pattern;
 public class PyramidNumberPattern {
     public static void main(String[] args){
         for(int i=1; i<=5; i++){
-            for(int j=1; j<=5-i; j++){
+            for(int j=1; j<=5-i; j++){//5-i determines how many spaces should be printed before the numbers in the current row.
                 System.out.print(" ");
             }for(int j=1; j<=i; j++){
                 System.out.print(" "+ i+" ");
