977361206e406d0be62809ee6e16fcbf3ad990b4
Pratibha <pc634805@gmail.com>
2024-06-12 23:31:35 +0530

    Java Program to Print Mirror Lower Star Triangle Pattern

--- Pattern/MirrorLowerStarTriangle.java
+++ Pattern/MirrorLowerStarTriangle.java
@@ -1,2 +1,36 @@
-package JAVA_Practice.Pattern;public class MirrorLowerStarTriangle {
+//Java Program to Print Mirror Lower Star Triangle Pattern
+//* * * * * *
+// * * * * *
+//  * * * *
+//   * * *
+//    * *
+//     *
+//     *
+//    * *
+//   * * *
+//  * * * *
+// * * * * *
+//* * * * * *
+package JAVA_Practice.Pattern;//problem
+
+public class MirrorLowerStarTriangle {
+    public static void main(String[] args){
+        for(int i=7; i>=1; i--){
+            for(int j=7; j>i; j--){
+                System.out.print(" ");
+            }
+            for(int j=1; j<i; j++){
+                System.out.print( "* ");
+            } System.out.println();
+        }
+        for(int i=1; i<=7; i++){
+            for(int j=1; j<=7-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<i; j++){
+                System.out.print( "* ");
+            } System.out.println();
+        }
+
+    }
 }
