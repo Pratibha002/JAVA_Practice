78ad26d2eec54f99230d586c42a0bc5c013c67fb
Pratibha <pc634805@gmail.com>
2024-06-12 23:33:51 +0530

    Java Program to Print hollow Square Star Pattern

--- Pattern/SquarePattern2.java
+++ Pattern/SquarePattern2.java
@@ -1,2 +1,24 @@
-package JAVA_Practice.Pattern;public class SquarePattern2 {
+//Java Program to Print hollow Square Star Pattern
+//   ************
+//   *          *
+//   *          *
+//   *          *
+//   ************
+package JAVA_Practice.Pattern;
+
+public class SquarePattern2 {
+    public static void main(String[] args){
+
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=12; j++){
+                if(i==1||i==5||j==1||j==12){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            } System.out.println();
+
+
+        }
+    }
 }
