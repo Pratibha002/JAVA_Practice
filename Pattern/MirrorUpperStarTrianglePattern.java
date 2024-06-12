1aecebf385168c41a37fee348ffaea72caadcd4a
Pratibha <pc634805@gmail.com>
2024-06-12 07:44:31 +0530

    Mirror Upper Star Triangle Pattern

--- Pattern/MirrorUpperStarTrianglePattern.java
+++ Pattern/MirrorUpperStarTrianglePattern.java
@@ -1,2 +1,23 @@
-package JAVA_Practice.Pattern;public class MirrorUpperStarTrianglePattern {
+package JAVA_Practice.Pattern;
+
+public class MirrorUpperStarTrianglePattern {
+    public static void main(String[] args){
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=5-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print( "* ");
+            }System.out.println();
+        }
+        for(int i=5; i>=1; i--){
+            for(int j=5; j>i; j--){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
