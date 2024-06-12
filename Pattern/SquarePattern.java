3a64e307ce3daf760aafbdb8b077c3addb0f8214
Pratibha <pc634805@gmail.com>
2024-06-12 23:34:55 +0530

    Java Program to Print Square Star Pattern

--- Pattern/SquarePattern.java
+++ Pattern/SquarePattern.java
@@ -1,2 +1,12 @@
-package JAVA_Practice.Pattern;public class SquarePattern {
+//Java Program to Print Square Star Pattern
+package JAVA_Practice.Pattern;
+
+public class SquarePattern {
+    public static void main(String[] args){
+           for(int i=1; i<=10; i++){
+               for(int j=1; j<=10; j++){
+                   System.out.print("* ");
+               }System.out.println();
+           }
+    }
 }
