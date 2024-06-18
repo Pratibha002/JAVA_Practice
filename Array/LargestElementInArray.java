547f73c8c081cb40562aaf8b999d0ce321949b99
Pratibha <pc634805@gmail.com>
2024-06-18 23:56:56 +0530

    Find the Largest Element in an Array

--- Array/LargestElementInArray.java
+++ Array/LargestElementInArray.java
@@ -1,2 +1,18 @@
-package JAVA_Practice.Array;public class LargestElementInArray {
+//Find the Largest Element in an Array
+package JAVA_Practice.Array;
+
+public class LargestElementInArray {
+    public static void main(String[] args){
+        int a[] = {20, 10, 20, 4, 100};
+        System.out.println("Given array : " );
+        int maxElement=a[0];
+        for(int i=0; i<a.length; i++){
+            System.out.print(a[i]+" ");
+           if(maxElement<a[i]){
+               maxElement=a[i];
+           }
+        }
+        System.out.println();
+        System.out.println("Maximum element of given array is : "+maxElement);
+    }
 }
