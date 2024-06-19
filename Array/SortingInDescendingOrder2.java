40037bef4a0215944c190a43e3bea61d1b4c7c53
Pratibha <pc634805@gmail.com>
2024-06-19 23:00:20 +0530

    Array/SortingInDescendingOrder2.java

--- Array/SortingInDescendingOrder2.java
+++ Array/SortingInDescendingOrder2.java
@@ -1,2 +1,15 @@
-package JAVA_Practice.Array;public class SortingInDescendingOrder2 {
+//Java Program to Sort the Array Elements in Descending Order
+package JAVA_Practice.Array;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SortingInDescendingOrder2 {
+    public static void main(String[] args) {
+        Integer[] a = {2, 6, 23, 98, 24, 35, 78};
+        //Here we use the methods of collection to sort the array in descending order.
+        Arrays.sort(a, Collections.reverseOrder());
+        System.out.println("Sorted Array in Descending order  : " +Arrays.toString(a));
+
+    }
 }
