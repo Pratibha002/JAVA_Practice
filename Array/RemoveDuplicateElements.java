93d2718e07a6426f32ee3eed79389504b8f49b65
Pratibha <pc634805@gmail.com>
2024-06-19 23:13:53 +0530

    Remove duplicates from Sorted Array

--- Array/RemoveDuplicateElements.java
+++ Array/RemoveDuplicateElements.java
@@ -1,2 +1,20 @@
-package JAVA_Practice.Array;public class RemoveDuplicateElements {
+package JAVA_Practice.Array;
+
+public class RemoveDuplicateElements {
+    public static void main(String[] args) {
+        int[] a= {1, 2, 2, 3, 4, 4, 4, 5, 5};
+        System.out.println("Unique Elements ");
+        for(int i=0; i< a.length; i++){
+            boolean duplicate=false;
+            for(int j=0; j<i; j++){
+                if(a[i]==a[j]){
+                    duplicate=true;
+                    break;
+                }
+            }if(!duplicate){
+                System.out.print(a[i]+" ");
+            }
+
+        }
+    }
 }
