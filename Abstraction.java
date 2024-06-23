83073b80b6bc758dbe6a30ae11b5f5e5926a93a5
Pratibha <pc634805@gmail.com>
2024-06-23 23:43:37 +0530

    Abstraction practice

--- Abstraction.java
+++ Abstraction.java
@@ -1,2 +1,27 @@
-package JAVA_Practice;public class Abstraction {
+package JAVA_Practice;
+
+import java.security.PublicKey;
+
+abstract class Vehicle{
+    public abstract  int getNoOfWheels();
+}
+class Bus extends Vehicle{
+    @Override
+    public int getNoOfWheels() {
+        return 6;
+    }
+}
+class Auto extends Vehicle{
+    @Override
+    public int getNoOfWheels() {
+        return 3;
+    }
+}
+public class Abstraction {
+    public static void main(String[] args){
+        Bus b =new Bus();
+        System.out.println("Number of wheels in a Bus = "+b.getNoOfWheels());
+        Auto a =new Auto();
+        System.out.println("Number of wheels in a Auto = "+a.getNoOfWheels());
+    }
 }
