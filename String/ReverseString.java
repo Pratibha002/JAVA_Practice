2a67f9b3364b025356ced249018775c48986ec99
Pratibha <pc634805@gmail.com>
2024-06-24 22:21:50 +0530

    Reverse a string in Java

--- String/ReverseString.java
+++ String/ReverseString.java
@@ -1,2 +1,13 @@
-package JAVA_Practice.String;public class ReverseString {
+package JAVA_Practice.String;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String s ="hello world";
+        System.out.println("original string : "+s);
+
+        System.out.println("Modified  string : ");
+        for(int i=s.length()-1; i>=0; i--){
+            System.out.print(s.charAt(i));
+        }
+    }
 }
