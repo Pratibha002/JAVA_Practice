7e50d151386277129120b86c9859749bf57ff866
Pratibha <pc634805@gmail.com>
2024-06-24 22:22:40 +0530

    Reverse a string in Java by using .reverse() method

--- String/ReverseString2.java
+++ String/ReverseString2.java
@@ -1,2 +1,11 @@
-package JAVA_Practice.String;public class ReverseString2 {
+//Reverse a string in Java by using .reverse() method
+package JAVA_Practice.String;
+
+public class ReverseString2 {
+    public static void main(String[] args) {
+        String s ="hello world";
+        System.out.println("Original String : "+s);
+        StringBuffer sb =new StringBuffer(s);
+        System.out.println("Reversed String : "+sb.reverse());
+    }
 }
