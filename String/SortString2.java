af1fc72a48e31f5a03fe22d3b83f5452648d3c04
Pratibha <pc634805@gmail.com>
2024-06-25 23:24:21 +0530

    Sort a String in Java

---
+++ String/SortString2.java
@@ -0,0 +1,20 @@
+//Sort a String in Java
+package JAVA_Practice.String;
+
+public class SortString2 {
+    public static void main(String[] args) {
+        String s ="helloworld";
+        char [] c =s.toCharArray();
+        char temp=0;
+        for(int i=0; i<c.length; i++){
+            for(int j=i+1; j<c.length; j++){
+                if(c[i]>c[j]){
+                    temp=c[i];
+                    c[i]=c[j];
+                    c[j]=temp;
+
+                }
+            }
+        }System.out.println("Sorted String : "+c);
+    }
+}
