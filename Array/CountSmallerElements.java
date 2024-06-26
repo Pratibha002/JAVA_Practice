31ec38e451e0cc493c8d7b07007b74ca07662599
Pratibha <pc634805@gmail.com>
2024-06-26 23:29:52 +0530

    Find number of elements which are less than or equal to given element X.

--- Array/CountSmallerElements.java
+++ Array/CountSmallerElements.java
@@ -1,2 +1,20 @@
-package JAVA_Practice.Array;public class CountSmallerElements {
+package JAVA_Practice.Array;
+
+import java.util.Arrays;
+
+public class CountSmallerElements {
+    public static void main(String[] args) {
+        int [] a = {1,3,4,5,6, 7, 8 ,9};
+        int x=7;
+        int count=0;
+        Arrays.sort(a);
+        for(int i=0; i<a.length; i++){
+            if(a[i]<=x){
+                count++;
+            }else {
+                break;
+            }
+        }
+        System.out.println(count);
+    }
 }
