faa26f44d915e932155b71976295735c9ae1161a
Pratibha <pc634805@gmail.com>
2024-06-26 23:27:15 +0530

    Rotation of an array

--- Array/ArrayRotation.java
+++ Array/ArrayRotation.java
@@ -1,2 +1,17 @@
-package JAVA_Practice.Array;public class ArrayRotation {
+package JAVA_Practice.Array;
+
+public class ArrayRotation {
+    public static void main(String[] args) {
+        int [] a ={2,4,5,7,8,35,77};
+        int d =2;
+        int temp=0;
+        for(int i=d; i<a.length; i++){  // desired o/p --> 5,7,8,35,77,2,4
+//            System.out.println(a[i]);
+            temp=a[i];
+            System.out.print(temp+ " ");
+        }
+        for(int i=0; i<d; i++){
+            System.out.print(a[i]+" ");
+        }
+    }
 }
