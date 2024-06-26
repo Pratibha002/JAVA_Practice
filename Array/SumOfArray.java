ab92bc02a20d59f8c8da5e3f98f88f520a46050f
Pratibha <pc634805@gmail.com>
2024-06-26 23:45:53 +0530

    Sum of array

--- Array/SumOfArray.java
+++ Array/SumOfArray.java
@@ -1,2 +1,12 @@
-package JAVA_Practice.Array;public class SumOfArray {
+package JAVA_Practice.Array;
+
+public class SumOfArray {
+    public static void main(String[] args) {
+        int [] a = {1,2,3,4,5,6,7,8,9,10};
+        int sum = 0;
+        for(int i=0; i<a.length; i++){
+            sum+=a[i];
+        }
+        System.out.println(sum);
+    }
 }
