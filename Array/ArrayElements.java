51f601e390359c6726c0ee402b3cac7aac161291
Pratibha <pc634805@gmail.com>
2024-06-30 23:15:25 +0530

    Given an array, the task is to find all elements in array except two greatest elements in sorted order.

--- Array/ArrayElements.java
+++ Array/ArrayElements.java
@@ -1,6 +1,7 @@
 //Given an array, the task is to find all elements in array except two greatest elements in sorted order.
 package JAVA_Practice.Array;
 
+
 public class ArrayElements {
     public static void main(String[] args) {
         int [] a ={1,2,3,4,5,6,7,8,9};
