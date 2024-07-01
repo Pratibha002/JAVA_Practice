617bfd28098c19d312c9ccd7164b90b4edaf8ea1
Pratibha <pc634805@gmail.com>
2024-07-01 23:44:52 +0530

    Check if a given string is Pangram in Java

--- String/PangramString.java
+++ String/PangramString.java
@@ -1,2 +1,24 @@
-package JAVA_Practice.String;public class PangramString {
+package JAVA_Practice.String;
+
+public class PangramString {
+    public static void main(String[] args) {
+        String s ="abcdefghijklmnopqrstuvwxyz".toLowerCase();
+        boolean [] b =new boolean[26];
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)>='a'&& s.charAt(i)<='z'){
+                int index = s.charAt(i)-'a';
+                b[index]=true;
+            }
+        }boolean isPangram=true;
+        for(boolean present:b){
+            if(!present){
+                isPangram=false;
+                break;
+            }
+        }if(isPangram){
+            System.out.println("Yes, it is a pangram!!");
+        }else{
+            System.out.println("no, it is not a pangram!!");
+        }
+    }
 }
