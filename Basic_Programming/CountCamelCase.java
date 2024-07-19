32e2b920aebc6e8fc2d21b50f735e2ea3f2fe674
Pratibha <pc634805@gmail.com>
2024-07-19 23:50:41 +0530

    Count the camel case

--- Basic_Programming/CountCamelCase.java
+++ Basic_Programming/CountCamelCase.java
@@ -1,2 +1,20 @@
-package JAVA_Practice.Basic_Programming;public class CountCamelCase {
+package JAVA_Practice.Basic_Programming;
+
+import java.util.Scanner;
+
+public class CountCamelCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String : ");
+       // String s = "heLLOjAvA";
+        String s = sc.nextLine();
+        int count=0;
+        for(int i=0; i<s.length(); i++){
+            char c =s.charAt(i);
+            if(Character.isUpperCase(c)){
+                  count++;
+            }
+        }
+        System.out.println("Count of uppercase character in given string is : "+count);
+    }
 }
