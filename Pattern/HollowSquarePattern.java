801f683a7b6e7b45c8fe99248511f646c969bd89
Pratibha <pc634805@gmail.com>
2024-07-28 23:10:35 +0530

    Hollow square pattern

--- Pattern/HollowSquarePattern.java
+++ Pattern/HollowSquarePattern.java
@@ -6,7 +6,7 @@
 //   ************
 package JAVA_Practice.Pattern;
 
-public class SquarePattern2 {
+public class HollowSquarePattern {
     public static void main(String[] args){
 
         for(int i=1; i<=5; i++){
