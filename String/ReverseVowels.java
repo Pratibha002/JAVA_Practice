0fb59522396b3759c032bfe4b57e084e68aa7df0
Pratibha <pc634805@gmail.com>
2024-08-05 21:17:39 +0530

    reverse the vowels

--- String/ReverseVowels.java
+++ String/ReverseVowels.java
@@ -1,2 +1,19 @@
-package JAVA_Practice.String;public class ReverseVowels {
+package JAVA_Practice.String;
+
+import java.util.Scanner;
+
+public class ReverseVowels {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the word/sentence : ");
+        String s =sc.nextLine();
+        StringBuilder result =new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            if((s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u')||
+                    ( s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U')){
+                result.append(s.charAt(i));
+                result.reverse();
+            }
+        }System.out.println("Reverse vowel of given string: "+result);
+    }
 }
