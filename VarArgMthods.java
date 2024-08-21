c735135bb5a1ce8c005701d795c6e5d128afcb57
Pratibha <pc634805@gmail.com>
2024-08-21 23:55:18 +0530

    Practice the VarArgMethod()

--- VarArgMthods.java
+++ VarArgMthods.java
@@ -1,2 +1,17 @@
-package JAVA_Practice;public class VarArgMthods {
+//we csn declare a method which can take variable number of arguments such type of methods are called var-arg methods
+package JAVA_Practice;
+
+public class VarArgMthods {
+     static void sum(int... x){
+        int total=0;
+        for(int i:x){
+            total=total+i;
+        }System.out.println(total);
+    }
+    public static void main(String[] args){
+        sum();
+        sum(10,20);
+        sum(10,20,30);
+        sum(10,20,30,40);
+    }
 }
