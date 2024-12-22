8ca1cf830b78f0760a16a641f408ac5dbe89c33b
Pratibha <pc634805@gmail.com>
2024-12-22 09:08:43 +0530

    find the middle character of a string

--- Basic_Programming/MiddleString.java
+++ Basic_Programming/MiddleString.java
@@ -1,2 +1,24 @@
-package JAVA_Practice.Basic_Programming;public class MiddleString {
+package JAVA_Practice.Basic_Programming;
+
+import java.util.Scanner;
+
+public class MiddleString {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String s = sc.nextLine();
+        int midCharNum;
+        if (s.length()%2==0){
+            midCharNum=s.length()/2;
+            midCharNum--;
+            System.out.print(s.charAt(midCharNum));
+            System.out.print(", "+s.charAt(midCharNum+1));
+
+        }else {
+            midCharNum=(s.length()+1)/2;
+            midCharNum--;
+            System.out.print(s.charAt(midCharNum));
+        }
+
+    }
 }
