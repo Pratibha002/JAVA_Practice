9950d2a2089f910abdb207a44282e98ece637e4e
Pratibha <pc634805@gmail.com>
2024-12-22 09:09:53 +0530

    Reverse every second word of a given Strirng

--- String/ReverseEverySecondWord.java
+++ String/ReverseEverySecondWord.java
@@ -1,2 +1,21 @@
-package JAVA_Practice.String;public class ReverseEverySecondWord {
+package JAVA_Practice.String;
+
+public class ReverseEverySecondWord {
+    public static void main(String[] args) {
+        String str ="hello my name is Pratibha Chauhan how are you";
+        //task is to reverse every 2nd string
+        String [] words =  str.split(" ");
+        StringBuilder result= new StringBuilder();
+
+        for(int i=0; i<words.length; i++){
+            if(i%2!=0){
+                String revWord = new StringBuilder(words[i]).reverse().toString();
+                result.append(revWord).append(" ");
+            }
+            else {
+                result.append(words[i]).append(" ");
+            }
+        }
+        System.out.println("Modified sentence: " + result.toString());
+    }
 }
