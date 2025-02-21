ae08223fcccdb9335dbb8521b2a430efde32ed40
Pratibha <pc634805@gmail.com>
2025-02-21 23:55:25 +0530

    right star triangle

--- Pattern/RightTriangleStarPattern.java
+++ Pattern/RightTriangleStarPattern.java
@@ -1,2 +1,25 @@
-package JAVA_Practice.Pattern;public class RightTriangleStarPatttern {
+/*
+      *
+     **
+    ***
+   ****
+  *****
+
+ */
+package JAVA_Practice.Pattern;
+
+public class RightTriangleStarPattern {
+    public static void main(String[] args) {
+        int n=5;
+        for (int i=1; i<=n; i++){
+            //inner loop for space
+            for (int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
