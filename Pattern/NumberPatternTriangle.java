31f9b69864b01cd5b8c4ae2e1cacf64132600c10
Pratibha <pc634805@gmail.com>
2025-02-21 23:51:13 +0530

    number pattern

--- Pattern/NumberPatternTriangle.java
+++ Pattern/NumberPatternTriangle.java
@@ -1,2 +1,22 @@
-package JAVA_Practice.Pattern;public class NumberPatternTriangle {
+/*
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+ */
+package JAVA_Practice.Pattern;
+
+public class NumberPatternTriangle {
+    public static void main(String[] args) {
+
+        for (int i=1; i<=5; i++){
+
+            for (int j=1; j<=i; j++){
+
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
 }
