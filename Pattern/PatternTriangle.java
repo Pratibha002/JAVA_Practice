9f5271a9c8fe7f762db57b287f5503990fbe7713
Pratibha <pc634805@gmail.com>
2025-02-21 23:52:00 +0530

    pattern

--- Pattern/PatternTriangle.java
+++ Pattern/PatternTriangle.java
@@ -1,2 +1,28 @@
-package JAVA_Practice.Pattern;public class Pattern {
+/*
+
+1
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
+
+ */
+
+package JAVA_Practice.Pattern;
+
+public class PatternTriangle {
+    public static void main(String[] args) {
+        int n =5;
+
+        for(int i=1; i<=n; i++){
+            for (int j=1; j<=i; j++){
+                if ((i+j) % 2 ==0){
+                    System.out.print("1"+ " ");
+                }else {
+                    System.out.print("0"+ " ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
