19457487ce5c839dda8224b5ce143c31ef054210
Pratibha <pc634805@gmail.com>
2025-02-21 23:53:31 +0530

    reverse number triangle

--- Pattern/ReverseNumberPatternTriangle.java
+++ Pattern/ReverseNumberPatternTriangle.java
@@ -1,2 +1,22 @@
-package JAVA_Practice.Pattern;public class ReverseNumberPatternTriangle {
+/*
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+ */
+package JAVA_Practice.Pattern;
+
+public class ReverseNumberPatternTriangle {
+    public static void main(String[] args) {
+
+        for (int i=5; i>=1; i--){
+
+            for (int j=1; j<=i; j++){
+
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
 }
